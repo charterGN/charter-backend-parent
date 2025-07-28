dc3786f4aae3a4ba595c7be007da1508614bfe4b
charter <2088694379@qq.com>
2025-07-28 23:23:17 +0800

    feat(admin): 添加菜单管理相关基础类
    添加DynamicMenuVO和MenuController基础类，为后续菜单管理功能开发做准备

---
+++ charter-server/charter-server-admin/src/main/java/ink/charter/website/server/admin/sys/controller/MenuController.java
@@ -0,0 +1,5 @@
+package ink.charter.website.server.admin.sys.controller;/**
+@author charter
+@create 2025/07/28
+*/public class MenuController {
+}
---
+++ charter-server/charter-server-admin/src/main/java/ink/charter/website/server/admin/sys/vo/menu/DynamicMenuVO.java
@@ -0,0 +1,5 @@
+package ink.charter.website.server.admin.sys.vo.menu;/**
+@author charter
+@create 2025/07/28
+*/public class DynamicMenuVO {
+}
