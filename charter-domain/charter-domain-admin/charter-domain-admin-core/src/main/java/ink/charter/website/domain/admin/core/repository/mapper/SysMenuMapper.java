8a93fd6f7f81e16ab7533a64686cb9e753663e40
周天富 <zhoutianfu@fulute.net>
2025-11-13 09:38:54 +0800

    update:菜单页排序修复

--- charter-domain/charter-domain-admin/charter-domain-admin-core/src/main/java/ink/charter/website/domain/admin/core/repository/mapper/SysMenuMapper.java
+++ charter-domain/charter-domain-admin/charter-domain-admin-core/src/main/java/ink/charter/website/domain/admin/core/repository/mapper/SysMenuMapper.java
@@ -30,6 +30,7 @@ public interface SysMenuMapper extends BaseMapper<SysMenuEntity> {
         return selectList(QueryWrappers.<SysMenuEntity>lambdaQuery()
                 .likeIfPresent(SysMenuEntity::getMenuName, pageRequest.getMenuName())
                 .eqIfPresent(SysMenuEntity::getStatus, pageRequest.getStatus())
+                .orderByDesc(SysMenuEntity::getSortOrder)
                 .orderByDesc(SysMenuEntity::getCreateTime));
     }
 
